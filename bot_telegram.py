00e0a8b98add1340f1be3a5bc57c54df3f393cc5
Hugo Penna <git@hugopenna.com>
2022-11-27 12:17:56 -0300

    Implementa comando /take - sem DB

--- bot_telegram.py
+++ bot_telegram.py
@@ -6,9 +6,18 @@ bot = telebot.TeleBot(TOKEN)
 
 cupons = []
 
+@bot.message_handler(commands=["take"])
+def cmd_take(message):
+    if cupons == []:
+        msg = "Opa, parece que nao temos nenhum cupom no momento."
+    else:
+        msg = """Pega esse cupom ae!
+{}""".format(cupons[0])
+
+    bot.send_message(message.chat.id, msg)
 
 @bot.message_handler(commands=["give"])
-def give_cupom(message):
+def cmd_give(message):
     print(message)
     msg = bot.send_message(message.chat.id, "mande seu cupom")
     bot.register_next_step_handler(msg, save_cupom)
@@ -17,7 +26,7 @@ def give_cupom(message):
 def save_cupom(message):
     cupons.append(message.text)
     print(cupons)
-    msg = bot.send_message(message.chat.id, """seu cupom foi salvo com sucesso, 
+    bot.send_message(message.chat.id, """seu cupom foi salvo com sucesso, 
 obrigado :)""")
 
 @bot.message_handler(func=lambda m: True)
