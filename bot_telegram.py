da63d89fe5fc30b81b03893090c7b46e5ca11acf
Hugo Penna <git@hugopenna.com>
2022-11-27 12:10:27 -0300

    Implementar comando /give - sem DB

--- bot_telegram.py
+++ bot_telegram.py
@@ -4,10 +4,21 @@ TOKEN = "5426919180:AAEyGzMQfGSjBwFZglCX04mm7jiuM9R_1Vg"
 
 bot = telebot.TeleBot(TOKEN)
 
+cupons = []
+
+
 @bot.message_handler(commands=["give"])
 def give_cupom(message):
     print(message)
-    bot.reply_to(message, "mande seu cupom")
+    msg = bot.send_message(message.chat.id, "mande seu cupom")
+    bot.register_next_step_handler(msg, save_cupom)
+
+
+def save_cupom(message):
+    cupons.append(message.text)
+    print(cupons)
+    msg = bot.send_message(message.chat.id, """seu cupom foi salvo com sucesso, 
+obrigado :)""")
 
 @bot.message_handler(func=lambda m: True)
 def text(message):
@@ -18,5 +29,6 @@ def text(message):
         /take - se estiver precisando de um cupom
     """
     bot.reply_to(message, WELCOME_MSG)
+
 
 bot.polling()
