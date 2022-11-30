770ab9a331555c70ef5de8468a1008eac20c2f1a
Hugo Penna <git@hugopenna.com>
2022-11-30 09:09:51 -0300

    WIP Create DB apanhando como se não houvesse amanhã.

--- create_db.py
+++ create_db.py
@@ -3,11 +3,14 @@ import sqlite3
 
 con = sqlite3.connect("main.db")
 cur = con.cursor()
-cur.execute("""CREATE TABLE cupons(
-    cupom_id, 
-    given_date DEFAULT NULL, 
-    given_by DEFAULT NULL, 
-    taken_date DEFAULT NULL, 
-    taken_by DEFAULT NULL
-    )""")
+cur.execute("""CREATE TABLE cupons (
+    cupom_id TEXT NOT NULL,
+    given_by TEXT DEFAULT NULL,
+    given_date TEXT DEFAULT NULL,
+    taken_by TEXT DEFAULT NULL,
+    taken_date TEXT DEFAULT NULL
+    );""")
 
+cur.execute("""INSERT INTO cupons VALUES 
+(cupom_id='236F06462E95', given_by='Hugopenna')
+""")
