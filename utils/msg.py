b241b50c2f211ccf93c12238feca0a969452cc0d
Hugo Penna <git@hugopenna.com>
2022-12-05 09:59:34 -0300

    WIP extraindo msgs para arquivo auxiliar

---
+++ utils/msg.py
@@ -0,0 +1,28 @@
+MSG_APOIO = """Tem sido divertido fazer esse bot.\nSe te ajudei de alguma forma,\ndeixe um café pra mim :)\n
+CHAVE PIX: <code>26f2cc7b-5c72-4319-a0f6-a75ddbdf0dc3</code>"""
+DISCLAIMER = """
+[Informação importante]
+
+Os cupons que o bot compartilha são de outros usuários.
+
+Se você não estiver precisando de um agora, mande-o de volta com /give
+
+Se te ajudou, considere me /apoiar"""
+
+not_cupom = 'Eita, parece que isso não é um cupom.'
+saved_cupom = """seu cupom foi salvo com sucesso,\n obrigado :)"""
+too_many_cupom = "Parece que vc já pegou cupons recentemente, vamos deixar para os outros amiguinhos também."
+without_cupom = "Opa, parece que nao temos nenhum cupom no momento."
+
+welcome_msg = """
+Bot para pegar e fornecer cupom do Husky.io
+
+Para comecar escolha um comando:
+    /give - para deixar um cupom
+    /take - para pegar um cupom
+    /apoiar - para apoiar o desenvolvimento
+
+Os cupons não são meus e nem infinitos, caso vc esteja só testando, mande o cupom de volta com /give
+
+Dúvidas, comentários e sugestões, chama o @hugopenna
+"""
